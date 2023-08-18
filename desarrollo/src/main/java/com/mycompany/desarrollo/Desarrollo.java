1132fb1215ec1efa82370ddbd4dfe9e0c3bd5f13
alexefrain17 <1432461@senati.pe>
2023-08-17 19:13:46 -0500

    agregue scaner

---
+++ desarrollo/src/main/java/com/mycompany/desarrollo/Desarrollo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.desarrollo;
+import java.util..Scanner;
+/**
+ *
+ * @author Alumno
+ */
+public class Desarrollo {
+
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+     int[] numeros1 = int[5];
+     
+    
+             
+         }
+}
